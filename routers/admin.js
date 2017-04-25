69d264d5b4baf310615e72d9692668c26a5f30a0
wh-Coder <461891475@qq.com>
2017-04-25 14:12:30 +0800

    15.分页

--- routers/admin.js
+++ routers/admin.js
@@ -19,14 +19,30 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.get('/user', (req,res) => {
-    User.find().then(function (users) {
-        res.render('admin/user_index', {
-            userInfo: req.userInfo,
-            users: users
+router.get('/user', (req, res) => {
+    let page = Number(req.query.page || 1);
+    let limit = 2;
+    let pages = 0;
+
+    User.count().then(function (count) {
+        pages = Math.ceil(count / limit);
+        page = Math.min(page, pages);
+        page = Math.max(page, 1);
+
+        let skip = (page - 1) * limit;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+
+                count: count,
+                pages: pages,
+                page: page,
+                limit: limit
+            });
         });
     });
-
 
 });
 
